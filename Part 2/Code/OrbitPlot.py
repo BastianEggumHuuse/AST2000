4c22fdde8298d8e54cefbb5820890d11af1c3a6b
Bendik <bendik.thune@gmail.com>
2025-09-10 14:45:13 +0200

    første utkast

--- Part 2/Code/OrbitPlot.py
+++ Part 2/Code/OrbitPlot.py
@@ -1,4 +1,6 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
 # AST imports
 import ast2000tools.constants as const
 import ast2000tools.utils as utils
@@ -7,7 +9,32 @@ from ast2000tools.solar_system import SolarSystem
 seed = utils.get_seed('bmthune')
 system = SolarSystem(seed)
 
-print(system.eccentricities[0])
-print(system.semi_major_axes[0])
-print(system.semi)
+#Henter data fra system
+a = system.semi_major_axes
+e = system.eccentricities
 
+angles = np.linspace(0,2*np.pi, 1000)
+
+
+r = np.zeros((len(a),2, len(angles)))
+for i in range(len(a)):        
+        r[i][0] = ((a[i]*(1-e[i]**2))/(1+e[i]*np.cos(angles))) * np.cos(angles)
+        r[i][1] = ((a[i]*(1-e[i]**2))/(1+e[i]*np.cos(angles))) * np.sin(angles)
+
+
+
+
+
+
+fig, ax = plt.subplots()
+# 1 = 1, 2= 2, 3= 5 , 4 = 4, 5= ?, 6= ? 7=3, 
+colours = ["red", 'darkorange', 'mediumblue', 'limegreen', 'purple', 'darkviolet', 'gold']
+#plot
+for i in range(len(r)):
+    p = r[i]
+    print(p[0],p[1])
+    ax.plot(p[0],p[1], color = colours[i])
+sol = plt.Circle((0, 0), 1, color = 'gold')
+plt.axis('equal')
+ax.add_patch(sol)
+plt.show()
