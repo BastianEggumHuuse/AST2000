c5827456b82aacf4ae0b91e1b9fba0b1ad9d7fa8
Bastian Eggum Huuse <48051835+BastianEggumHuuse@users.noreply.github.com>
2025-09-23 19:19:49 +0200

    fft

--- Part 2/Code/FFT.py
+++ Part 2/Code/FFT.py
@@ -6,39 +6,6 @@ import  ast2000tools.constants as const
 
 seed = utils.get_seed('bmthune')
 system = SolarSystem(seed)
-"""""
-print('My system has a {:g} solar mass star with a radius of {:g} kilometers.'
-      .format(system.star_mass, system.star_radius))
-
-for planet_idx in range(system.number_of_planets):
-    print('Planet {:d} is a {} planet with a semi-major axis of {:g} AU and mass {:g}, radius = {:g}.'
-          .format(planet_idx, system.types[planet_idx], system.semi_major_axes[planet_idx], system.masses[planet_idx]*1.989e+30/5.972e+24, system.radii[planet_idx]*1000/const.AU))
-
-
-array = np.array([[1,1],[2,2],[3,3],[4,5],[6,7]])
-array2 = np.array([[1,1],[2,2],[3,3],[4,2],[6,7]])
-print(array/array2)
-"""""
-#system.print_info()
-
-
-
-
-N = 2**10
-T_tot = 10
-sr = N/T_tot
-n = np.linspace(0, T_tot, N)
-x =  (
-    1.0*np.sin(2*np.pi*1*n) +   # 1 Hz
-    0.6*np.sin(2*np.pi*3*n) +   # 3 Hz
-    0.3*np.sin(2*np.pi*7*n)     # 7 Hz
-    
-)
-for i in range(len(x)):
-      x[i] += np.random.normal(0,max(x)/5)
-
-plt.plot(n,x)
-plt.show()
 
 def ditfft(X, N ):
       if N == 1:
@@ -59,17 +26,23 @@ def ditfft(X, N ):
             F[k + int(N/2)] = (p -q)
             
       return F
-f = ditfft(x,N)
-N = np.arange(len(f))
-freq = N/T_tot
-print(max(freq))
-n_halv = len(N)//2
-fre_halv = freq[:n_halv]
-F_halv = f[:n_halv]
 
+FileName = "SolarOrbitData.npz"
+Data = np.load(FileName)
 
-plt.plot(fre_halv,abs(F_halv))
+NoiseData = Data["v_noise"]
+RawData   = Data["v_raw"]
+TotalTime = Data["time"][0]
+N = 2
+while N*2 < len(NoiseData):
+      N *= 2
+      
+TimeSample = np.linspace(0,TotalTime,len(NoiseData))[:N]
+NoiseSample = RawData[:N]
+f = ditfft(NoiseSample,N)
+
+f_range = np.arange(len(f))
+frequency = f_range/TimeSample[-1]
+
+plt.plot(frequency,abs(f))
 plt.show()
-
-
-    
