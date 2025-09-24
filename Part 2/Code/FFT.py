6202e1b6a89a28bf723357edf9e2bd97e04abf8a
Bastian Eggum Huuse <48051835+BastianEggumHuuse@users.noreply.github.com>
2025-09-24 12:31:51 +0200

    straight up fixing it

--- Part 2/Code/FFT.py
+++ Part 2/Code/FFT.py
@@ -7,25 +7,24 @@ import  ast2000tools.constants as const
 seed = utils.get_seed('bmthune')
 system = SolarSystem(seed)
 
-def ditfft(X, N ):
+def ditfft(X, N):
+
       if N == 1:
             return X
             
       
-      F = np.zeros(N, dtype=np.complex128)
-      
-      even = ditfft(X[:N:2], int(N/2))
-      odd = ditfft(X[1:N:2], int(N/2))
-      
+      e = ditfft(X[:N:2], int(N/2))
+      o = ditfft(X[1:N:2], int(N/2))
+
       for k in range (0, N//2):
-            p = even[k]
-            q = np.exp(-1j *2 *np.pi/(N) *k)*odd[k]
+            p = e[k]
+            q = np.exp(-1j *2 *np.pi/(N) *k)*o[k]
             
-            F[k] = (p + q)
+            X[k] = (p + q)
             
-            F[k + int(N/2)] = (p -q)
+            X[k + int(N/2)] = (p -q)
             
-      return F
+      return X
 
 FileName = "SolarOrbitData.npz"
 Data = np.load(FileName)
@@ -33,16 +32,26 @@ Data = np.load(FileName)
 NoiseData = Data["v_noise"]
 RawData   = Data["v_raw"]
 TotalTime = Data["time"][0]
-N = 2
-while N*2 < len(NoiseData):
-      N *= 2
-      
+
+v_pec = np.mean(RawData)
+RawData -= v_pec
+N = 2**15
+
+Plateau = np.ones(len(NoiseData))
+ratio = 0.05
+Indexes = np.arange(int(np.floor(ratio * len(Plateau))))
+LerpersIn = np.sin(np.linspace(0,np.pi/2,len(Indexes)))
+Plateau[Indexes] *= LerpersIn
+Plateau[len(Plateau) - Indexes - 1] = LerpersIn
+
+plt.plot(np.linspace(0,1,len(NoiseData)),Plateau)
+
 TimeSample = np.linspace(0,TotalTime,len(NoiseData))[:N]
-NoiseSample = RawData[:N]
+NoiseSample = RawData[:N].astype(np.complex128)
 f = ditfft(NoiseSample,N)
 
 f_range = np.arange(len(f))
 frequency = f_range/TimeSample[-1]
 
-plt.plot(frequency,abs(f))
+#plt.plot(frequency,abs(f))
 plt.show()
