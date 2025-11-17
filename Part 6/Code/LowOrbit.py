8b6084fea1f5be4fdc7c22104cb236ee7f4ecfc3
Bendik <bendik.thune@gmail.com>
2025-11-17 14:41:20 +0100

    hhhmmmmmm

--- Part 6/Code/LowOrbit.py
+++ Part 6/Code/LowOrbit.py
@@ -22,36 +22,53 @@ landing = mission.begin_landing_sequence()
 landing.verbose = False
 
 t,r_0,v_0 = landing.orient()
-v_hat = v_0 / np.linalg.norm(v_0)
-dv = -200 * v_hat
-
-N = 10
-dv_i = dv/N
 
 
+v = np.sqrt((const.G*mission.system.masses[1]*const.m_sun/np.linalg.norm((r_0))))
+v_hat = -np.array((r_0[1], -r_0[0],0))/np.linalg.norm(r_0)
 
-N = 10
+dv = v*v_hat - v_0
+
+print(r_0, v*v_hat ,v_0*(60*60*24*365)/const.AU)
+landing.boost(dv)
+
+landing.look_in_direction_of_motion()
+landing.start_video()
+
+N = 1000
 r_mean = []
+r_ø = 2.7e6
 for n in range(N):
-    print(f"Running generation {n}")
 
-    I = 10
+    I = 1
     r_mean_i = 0
     for i in range(I):
-        landing.fall(3600*24 * 3.5)
+        landing.fall(60)
         t,r,v = landing.orient()
-        r_mean_i += np.linalg.norm(r) 
-
+        r_mean_i += np.linalg.norm(r)
+    if np.linalg.norm(r) < r_ø:
+        v_s = np.sqrt((const.G*mission.system.masses[1]*const.m_sun/np.linalg.norm((r))))
+        v_hat = -np.array((r[1], -r[0],0))/np.linalg.norm(r)
+        dv = v_s*v_hat - v
+        print(dv)
+        landing.boost(dv)
+       
+        break
     r_mean_i *= 1/I
 
     r_mean.append(r_mean_i)
-    print(f"Mean for gen {n} is {r_mean_i}")
+    v_hat = v / np.linalg.norm(v_0)
+    dv = -1000 * v_hat
+    dv_i = dv/N
     landing.boost(dv_i)
-    
-for r in r_mean:
 
-    print(f"mean r : {r:10.3f}")
+landing.fall(30000)
+landing.finish_video()
 
+
+
+with open ("Mission.pkl", 'wb') as file:
+    pkl.dump(mission, file)
 
 plt.plot(np.arange(len(r_mean)),np.ones(len(r_mean))*np.linalg.norm(r_0))
 plt.plot(np.arange(len(r_mean)),r_mean)
