5b81af96dba9e986114ef10be4d2a845739bb5ef
Bastian Eggum Huuse <48051835+BastianEggumHuuse@users.noreply.github.com>
2025-11-17 15:25:37 +0100

    Landing prep

--- Part 5/Code/TrueLaunch.py
+++ Part 5/Code/TrueLaunch.py
@@ -1,6 +1,3 @@
-# BRUKER IKKE KODEMAL
-# Skrevet av Bastian Eggum Huuse og Bendik Thune
-
 # Regular imports
 import numpy             as np
 import matplotlib.pyplot as plt
@@ -12,12 +9,14 @@ from CalculatingTra import Main as Coast
 # AST imports
 import ast2000tools.constants as const
 import ast2000tools.utils     as utils
+
 from ast2000tools.space_mission import SpaceMission
 from GeneralizedLaunch import NumericalOrbitFunction
 
 #Unpickling launch
 with open("Mission.pkl", 'rb') as file:
     mission = pkl.load(file)
+
 
 t = mission.time_after_launch
 fuelMass = 3535.634180709576
@@ -42,8 +41,6 @@ M = np.zeros(mission.system._number_of_planets + 1)
 M[:-1] = mission.system.masses
 M[-1] = mission.system.star_mass
 FindR = NumericalOrbitFunction(FilePath)
-
-N_Steps = 100
 
 # List of boost times (simulation)
 boost_t_sim = np.array([
@@ -83,8 +80,6 @@ boost_v_launch = np.array([
 travel = mission.begin_interplanetary_travel()
 travel.restart()
 
-vs = []
-rs = []
 
 for i in range(len(boost_t_launch)-1):
 
@@ -92,9 +87,6 @@ for i in range(len(boost_t_launch)-1):
     
     # Orienting ourselves
     t_o,R_o,V_o = travel.orient()
-
-    vs.append([V_o,V_sim[-1]])
-    rs.append([R_o,R_sim[-1]])
 
     # Updating launch data
     R_launch = np.concatenate((R_launch,np.array([R_o])))
@@ -105,7 +97,7 @@ for i in range(len(boost_t_launch)-1):
     # Boosting sim
     if(i < len(boost_t_sim) - 1):
         V_sim[-1] += boost_v_sim[i]
-
+    
     # Coasting
     travel.coast(boost_t_launch[i+1])
     # Coasting sim
@@ -125,14 +117,10 @@ t_o,R_o,V_o = travel.orient()
 R_launch = np.concatenate((R_launch,np.array([R_o])))
 V_launch = np.concatenate((V_launch,np.array([V_o])))
 
-# for i in range(10):
-#     print(f"Time-step {i}")
-#     print("Position: ")
-#     print(f"orientation : {rs[i][0]} | simulation : {rs[i][1]}")
-#     print("Velocity: ")
-#     print(f"orientation : {vs[i][0]} | simulation : {vs[i][1]}")
+# Plotting
 
 a_launch_0 = (V_launch[2] - V_launch[1])/dt
+print("acc_0", a_launch_0)
 
 
 fig, ax = plt.subplots()
@@ -142,7 +130,7 @@ ax.plot(R_planets[0][1],R_planets[1][1])
 R_p = FindR(t_o, 1)
 ax.plot(R_p[0], R_p[1], 'o', color = 'blue' )
 ax.plot(R_sim[:,0],R_sim[:,1], color = 'red')
-print(R_sim)
+
 ax.plot(R_launch[:,0],R_launch[:,1], '.', color = 'green')
 
 l = np.linalg.norm(R_launch[-1][-1])*(M[1]/(M[-1]*10))**0.5
@@ -153,22 +141,16 @@ ax.add_patch(OrbitRadi)
 r = R_o - R_p
 
 r_hat = r/np.linalg.norm(r)
-e = np.array([r_hat[1],-r_hat[0]])
-
-print(f'fuel{((const.G_sol * mission.system.masses[1]/(np.linalg.norm(r)))**0.5) * e + FindR.GetVelocity(t_o, 1)}')
-
+e = -np.array([r_hat[1],-r_hat[0]])
 
 v_stable = ((const.G_sol * mission.system.masses[1]/(np.linalg.norm(r)))**0.5) * e + FindR.GetVelocity(t_o, 1)
-dV = -(v_stable - V_o) 
-print("Stable: ",(v_stable - FindR.GetVelocity(t_o, 1)) * (const.AU / (60*60*24*365)))
+dV = (v_stable - V_o) 
 print(dV)
 travel.boost(dV)
 
 t_o,R_o,V_o = travel.orient()
 
-print(f'possisjonen er {R_o} og farten er {V_o} og tider er {t_o}')
+travel.record_destination(1)
 
-plt.xlabel("Distanse langs x-aksen [AU]")
-plt.ylabel("Distanse langs y-aksen [AU]")
-plt.title("Rakettbaner simulert (grønn)\nog gjennomført (rød)")
-plt.show()
+with open ("Mission.pkl", 'wb') as file:
+    pkl.dump(mission, file)
--- Part 6/Code/LandingPreparation.py
+++ Part 6/Code/LandingPreparation.py
@@ -81,6 +81,44 @@ if __name__ == "__main__":
 
     # Beginning landing sequence
     landing = mission.begin_landing_sequence()
+    landing.look_in_direction_of_planet(1)
 
     # Getting initial conditions
     t_0,r_vec_0,v_vec_0 = landing.orient()
+
+    # Getting planet spin
+    p_theta = (2*np.pi)/(mission.system.rotational_periods[1]*24*60*60)
+
+    d_t = (60*60*24)*0.03
+    coords_list = []
+
+    thetas = []
+
+    N = 10
+    for n in range(N):
+
+        # Finding the position at the current time
+        r = mission.system.radii[1] * 1000
+        theta,t = FindAngle(landing)
+
+        # Defining our coordinate vector
+        coord_vector = np.array((r,theta,0))
+
+        # Finding the vector at time = 0
+        theta_0 = AngleAtZero(coord_vector,t,p_theta)
+        coord_vector_0 = np.array((r,theta_0,0))
+
+        # saving data
+        coord_info = (coord_vector,coord_vector_0,t)
+        coords_list.append(coord_info)
+
+        # Taking photo
+        landing.take_picture(f"Preparation_image_at_time_{t}")
+
+        # Updating position
+        landing.fall(d_t)
+
+        thetas.append(theta)
+
+    plt.plot(range(len(thetas)),np.sin(thetas))
+    plt.show()
