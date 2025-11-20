6c74561867105110cc070323aebb74df3a7f8446
Bendik <bendik.thune@gmail.com>
2025-11-20 16:04:05 +0100

    n

--- Part 7/Code/LandingSimulation.py
+++ Part 7/Code/LandingSimulation.py
@@ -23,7 +23,8 @@ class LandingSimulation:
 
         self.mass            = mission.lander_mass
         self.lander_area     = mission.lander_area
-        self.parachute_area  = 12.794178689011185 # m^2
+        self.lander_limit    = 1e7 #Pa
+        self.parachute_area  = 13 # m^2
         self.parachute_limit = 250000 # N
         self.planet_radius   = mission.system.radii[1]*1000
         self.planet_mass     = mission.system.masses[1] * const.m_sun
@@ -57,7 +58,7 @@ class LandingSimulation:
         print("Density Initialized.\n")
 
     def _drag_velocity(self,r,v):
-        return v + (((r[0]**2 + r[1]**2)**0.5) * self.planet_rotation) * (np.cross(r,self.z_hat)/np.linalg.norm(r))
+        return v - (((r[0]**2 + r[1]**2)**0.5) * self.planet_rotation) * (np.cross(r,self.z_hat)/np.linalg.norm(r))
 
 
     def _compute_acceleration(self,r,v):
@@ -75,10 +76,15 @@ class LandingSimulation:
                 print("The parachute is now broken.")
                 self.parachute_broken = True
                 self.parachute_open   = False
-
+            
         else:           
             a_d = ((0.5 * soloutions.Rho(np.linalg.norm(r),self.r_limit,self.T_limit) * self.lander_area * np.linalg.norm(v_d)**2)/self.mass) * -v_d_hat
         
+        P_d = ((0.5 * soloutions.Rho(np.linalg.norm(r),self.r_limit,self.T_limit) * np.linalg.norm(v_d)**2)/self.mass)
+        
+        if P_d > self.lander_limit:
+            self.final_t = self.t + n*self.dt
+            self.burn()
         # Computing gravitational acceleration
         a_g = ((const.G * self.planet_mass)/np.linalg.norm(r)**2) * (-r/np.linalg.norm(r))
 
@@ -110,16 +116,19 @@ class LandingSimulation:
             # Exit clause
             if(np.linalg.norm(R[n]) <= self.planet_radius and self.landed == False):
                 self.final_t = self.t + n*self.dt
-                if(np.linalg.norm(V[n]) < 3):
-                    self.land(f"Lander has hit the ground with velocity {np.linalg.norm(V[n])}.")
-                else:
-                    self.crash(f"Lander has hit the ground with velocity {np.linalg.norm(V[n])}.")
+                Ground_velocity = V[n] - (((R[n][0]**2 + R[n][1]**2)**0.5) *self.planet_rotation) * (np.cross(R[n],self.z_hat)/np.linalg.norm(R[n]))
 
+                if(np.linalg.norm(Ground_velocity) < 3):
+                    self.land(f"Lander has hit the ground with velocity {np.linalg.norm(Ground_velocity)}.")
+                else:
+                    self.crash(f"Lander has hit the ground with velocity {np.linalg.norm(Ground_velocity)}.")
+
+            
             if(self.landed):
                 R[n+1] = np.array([0,0,self.planet_radius])
                 V[n+1] = np.zeros(3)
                 A[n+1] = np.zeros(3)
-
+            
             else:
                 self._time_step(R,V,A,n)
 
@@ -137,6 +146,9 @@ class LandingSimulation:
 
     def boost(self,dv):
         self.V[-1] += dv
+    def burn(self):
+        print(f"You burned to a crisp t = {self.final_t}, sim_time {self.final_t - self.t_0}")
+        self.landed = True
 
     def land(self,message):
         print(f"A landing has occured at t = {self.final_t}, sim_time {self.final_t - self.t_0}")
@@ -160,8 +172,8 @@ if __name__ == "__main__":
         landing = pkl.load(file)
 
     t_0,r_0,v_0 = landing.orient()
-    v_0 = np.array([0,0,0])
-
+    v_0 = np.array([1,0,0])#(2*np.pi) / (mission.system.rotational_periods[1]*24*60*60)*(np.cross(r_0,np.array([0,0,1])))#np.array([0,0,0])
+    print(np.linalg.norm(v_0))
     # Initializing simulation
     LandingSim = LandingSimulation(
         mission,
@@ -171,13 +183,13 @@ if __name__ == "__main__":
         )
     
     # Running simulation
-    LandingSim.fall(900)
+    LandingSim.fall(800)
     LandingSim.open_parachute()
-    LandingSim.fall(10000)
+    LandingSim.fall(3000)
 
     # Plotting
     ax = plt.axes()
-    ax.plot(np.linspace(0,LandingSim.sim_t,len(LandingSim.R)),np.linalg.norm(LandingSim.R,keepdims=True,axis = 1) - mission.system.radii[1]*1000)
+    ax.plot(np.linspace(0,LandingSim.sim_t,len(LandingSim.R)),np.linalg.norm(LandingSim.V,keepdims=True,axis = 1))
     plt.show()
     ax = plt.axes()
     ax.plot(LandingSim.R[:,0],LandingSim.R[:,1])
