c676f907705d46c5d90a938d5d311b632655146a
Bastian Eggum Huuse <Bastian.Eggum.Huuse@gmail.com>
2025-11-28 03:43:47 +0100

    1

--- Part 6/Code/SpectralAnalysis.py
+++ Part 6/Code/SpectralAnalysis.py
@@ -107,17 +107,16 @@ def ChiRange(fluxes, noises, L_0,M):
     
         flux_range = fluxes[i_l_min:i_l_max]
         noise_range = noises[i_l_min:i_l_max]
-        S = np.linspace(Sigma_l(L_0[i],k,T_min,M[i]),Sigma_l(L_0[i],k,T_max,M[i]),N_T)
+        #S = np.linspace(Sigma_l(L_0[i],k,T_min,M[i]),Sigma_l(L_0[i],k,T_max,M[i]),N_T)
 
         print(f"Running optimization for lambda_0 : {L_0[i]:5}")
         O[i] = ChiSquare(flux_range,noise_range,L_0[i],M[i],T,F_min)
-        #O[i] = ChiSquareSigma(flux_range,noise_range,S,F_min)
 
     return O
 
 @njit
 def FindLambda(l_0,length):
-    d_l = (1/3) * l_0 * 10**(-3)
+    d_l = (1/3) * l_0 * 10**(-4)
 
     l_max = l_0 + d_l
     l_min = l_0 - d_l
@@ -177,12 +176,7 @@ if __name__ == "__main__":
  
     # Files we will extract data from.
     noise_path = "sigma_noise.txt"
-    flux_path = "spectrum_seed83_600nm_3000nm.txt"
-
-    """
-    This way of reading the data is very very slow
-    so maybe we change it ?
-    """
+    flux_path = "spectrum_seed78_600nm_3000nm.txt"
 
     fluxes,noises = ReadLists(flux_path,noise_path,10)
 
@@ -216,10 +210,55 @@ if __name__ == "__main__":
         44
         ]) * const.m_p
     
+    print("\n---- Printing a few (lambda,F,sigma) triplets ----")
+    N = 10
+    Grain = 10001
+    for i in range(Grain,N * (Grain+1),Grain):
+        print(f"Lambda : {fluxes[i][0]:.5e}, F : {fluxes[i][1]:.5e}, sigma : {noises[i][1]}")
+    print("---- ---------------------------------------- ----\n")
+
+    # Plotting lambdas
+    plt.plot(fluxes[:,0][:4000],fluxes[:,1][:4000],color = "limegreen")
+    plt.axhline(1,color = "firebrick",linestyle = "--")
+    plt.xlabel(r"Bølgelengde $\lambda$ [nm]")
+    plt.ylabel(r"Relativ Fluks")
+    plt.show()
+
+    # Plotting sample spectra
+    N = 1000
+    X = np.linspace(0,1,N)
+    x = 0.35
+    def ColorMap(map,x):
+        c = (x - np.min(x)) / (np.max(x) - np.min(x))
+        return plt.get_cmap(map)(c)
+    def GaussMap(c,mu,sigma,strength = 1):
+        c_0 = c
+        a = np.ones(len(c))
+        for i in range(len(a)):
+            x_i = i / len(a)
+            y_i = ((2*np.pi*sigma**2)**0.5)*(1/((2*np.pi*sigma**2)**0.5)) * np.exp(-((x_i-mu)**2)/(2*sigma**2))
+            a[i] = 1 - y_i
+            if(a[i] < 0):
+                a[i] = 0
+
+        c_0[:,3] = a
+        return c_0
+    fig = plt.figure(figsize=(10, 8), dpi=80)
+    ax = plt.axes()
+    ax.bar(X,np.ones(N),width= 0.01,color = "k")
+    colors = GaussMap(ColorMap("hsv",X),x,0.02)
+    ax.bar(X,1,width= 0.01,color = colors) 
+    ax.set_aspect(0.25)
+    fig.tight_layout()
+    plt.xticks([0,x,1],["",r"$\lambda_{center}$",""])
+    plt.yticks([0,1],["",""])
+    plt.show()
+
     print("Running Optimization")
     O = ChiRange(fluxes,noises,L_0,M)
 
     print("")
 
     for i in range(len(O)):
-        print(f"Optimization for lambda_0 = {L_0[i]:5} : lambda = {O[i][0]:25} nm, T = {O[i][1]:25} K, F_min = {O[i][2]:25}")
+        print(f"Optimization for lambda_0 = {L_0[i]:5} : lambda = {O[i][0]:10.3f} nm, delta_lambda = {O[i][0] - L_0[i]:10.7f}, T = {O[i][1]:10.1f} K, F_min = {O[i][2]:20.3f}")
+    print("")
